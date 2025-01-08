4580a37ec51d3efa04d02a6b4e4f9526c3b8ff66
Nico Glas <info@nicoglas.com>
2025-01-08 23:05:59 +0100

    feat: publishing to githubpages

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
 ﻿using Devlead.Statiq.Themes;
+using Statiq.Web;
 
 return await Bootstrapper
     .Factory
     .CreateDefault(args)
     .AddThemeFromUri(new Uri("https://github.com/statiqdev/CleanBlog/archive/refs/heads/main.zip"))
     .AddWeb()
+    .DeployToGitHubPages("descalon","descalon.github.io", Config.FromSetting<string>("GITHUB_TOKEN"))
     .RunAsync();
 
